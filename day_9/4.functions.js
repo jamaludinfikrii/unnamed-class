326c861bc80af8395d64b1f18a13640bb25dd601
jamaludin.fiqri <jamaludin.fiqri@bukalapak.com>
2023-06-15 22:13:02 +0700

    node js

---
+++ day_9/4.functions.js
@@ -0,0 +1,36 @@
+
+// Function (input1, input2) ==> sesuatu
+
+
+// hitungUmur(2000) ==> 23
+// hitungUmur(1999) ==> 24
+function hitungUmur(tahunLahir) {
+  var umur = 2023 - tahunLahir
+  return `Lahir tahun ${tahunLahir}, umur sekarang ${umur}`
+}
+
+console.log(hitungUmur(1945))
+console.log(hitungUmur(1999))
+
+// parkir tiap jam 2000
+// hitungJamParkir(2) ==> 4000
+
+function hitungJamParkir(lamaParkir) {
+  var totalHarga = lamaParkir * 2000
+  return `total parkir = ${totalHarga}`
+}
+
+console.log(hitungJamParkir(5))
+
+
+
+// TUGAZZZZZ
+// hitungAlphabet('abc') ==> 1 +  2 + 3 = 6
+// hitungAlphabet('cda') ==> 3 + 4 + 1 = 8
+
+// hitungHari('senin',2) ==> rabu
+// hitungHari('minggu',3) ==> rabu
+
+// alphaForward('abc',2) ==> cde
+// alphaForward('kanzun',1) ==> lbmavm
+
