135c336e6afde43ac52835852fe96c441021d5e1
jamaludin.fiqri <jamaludin.fiqri@bukalapak.com>
2023-07-05 20:16:12 +0700

    review

---
+++ day_11/8.review.js
@@ -0,0 +1,19 @@
+// Review
+
+/**
+ * Variable
+ * Type data
+ * Operators, aritmetika, comparison
+ * Pengkondisian
+ * Pengulangan
+ * Arrays => CRUD
+ * Function => block of code for reusablity
+ * Object => CRUD
+ * ES 6 => fitur fitur baru
+ * Class => Besok
+ * Github => Besok
+ * Module 2
+ * Html
+ * Css
+ * React js
+ */
